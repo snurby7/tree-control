827fcb6a0fd7d3010ed9e78cd68f31b7d00d7466
Bruns <ryan.bruns7@gmail.com>
2018-12-12 14:49:08 -0500

    add option quick

--- src/app/tree-control/tree-options.interface.ts
+++ src/app/tree-control/tree-options.interface.ts
@@ -2,6 +2,7 @@ export interface ITreeOptions {
   dataSource: Object;
   showComboBox: boolean;
   isViewOnly: boolean;
+  maxNodeLevel?: number;
   onFilterChange?: (dataSource: any, filterText: string) => Object;
   minimumCharactersBeforeFilter?: number;
   hideExpandAll?: boolean;
